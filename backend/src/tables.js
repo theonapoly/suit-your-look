1079b2b6ebd8cf8a42f1d4553f95134e01f28ef6
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2024-01-21 17:23:34 +0100

    add copy of suit-your-look project hackathon 2

---
+++ backend/src/tables.js
@@ -0,0 +1,46 @@
+/* ************************************************************************* */
+// Register Data Managers for Tables
+/* ************************************************************************* */
+
+// Import the manager modules responsible for handling data operations on the tables
+
+const UserManager = require("./models/UserManager");
+const modelsManager = require("./models/modelsManager");
+const ProductManager = require("./models/ProductManager");
+const modelsProductsManager = require("./models/modelsProductsManager");
+const FavoriteManager = require("./models/FavoriteManager");
+
+const managers = [
+  modelsManager,
+  ProductManager,
+  UserManager,
+  modelsProductsManager,
+  FavoriteManager,
+];
+
+// Create an empty object to hold data managers for different tables
+const tables = {};
+
+// Register each manager as data access point for its table
+managers.forEach((ManagerClass) => {
+  const manager = new ManagerClass();
+
+  tables[manager.table] = manager;
+});
+
+/* ************************************************************************* */
+
+// Use a Proxy to customize error messages when trying to access a non-existing table
+
+// Export the Proxy instance with custom error handling
+module.exports = new Proxy(tables, {
+  get(obj, prop) {
+    // Check if the property (table) exists in the tables object
+    if (prop in obj) return obj[prop];
+
+    // If the property (table) does not exist, throw a ReferenceError with a custom error message
+    throw new ReferenceError(
+      `tables.${prop} is not defined. Did you register it in ${__filename}?`
+    );
+  },
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+/*eslint-disable*/
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
